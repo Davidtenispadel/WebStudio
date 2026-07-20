f484aaf4a133e7e4a627de5b544b6b5a371b21fa
Davidtenispadel <internet@dbsdesigner.com>
2026-07-20 09:39:20 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import Studio from './Studio';
-import SolarPanelCalculator from './components/SolarPanelCalculator';
-import BatteriesPage from './components/BatteriesPage';
 
-const App: React.FC = () => (
-  <Routes>
-    <Route path="/solar-calculator" element={<SolarPanelCalculator />} />
-    <Route path="/batteries" element={<BatteriesPage />} />
-    <Route path="*" element={<Studio />} />
-  </Routes>
-);
+const App: React.FC = () => <Studio />;
 
 export default App;
